42637d20ef453f50fe1f70ca0333f4167fa30d8f
Roman Lytvynenko <litvinenko95@gmail.com>
2018-03-07 16:10:32 +0200

    abstract buttons group and switcher semantic refactoring

--- src/components/AbstractButtonsGroup.js
+++ src/components/AbstractButtonsGroup.js
@@ -1,26 +1,36 @@
+/**
+ * Abstract component for buttons group.
+ *
+ * It can be used as a base for different cases when
+ * few buttons used as a same component (switcher, group).
+ *
+ * @constructor
+ */
 export default function CfAbstractButtonsGroup (Vue) {
-    return Vue.extend({
-        props: {
-            /**
-             * Possible states of switcher. This is
-             * an object with keys is a value of v-model
-             * and values are switcher titles.
-             */
-            states: {
-                type: Object,
-                required: true
-            }
-        },
+  return Vue.extend({
+    props: {
+      /**
+       * All buttons that should be rendered. This is
+       * an object where keys is button id and value is button's title.
+       */
+      buttons: {
+        type: Object,
+        required: true
+      }
+    },
 
-        methods: {
-            /**
-             * Set new state for this component.
-             *
-             * @param newState
-             */
-            setState (newState) {
-                this.$emit('button-clicked', newState);
-            }
-        }
-    })
+    methods: {
+      /**
+       * Button click event handler.
+       *
+       * Emmit event `button-clicked` with corresponding button identifier.
+       * Button's identifier is a key in `states` property.
+       *
+       * @param buttonId
+       */
+      buttonClick (buttonId) {
+        this.$emit('button-clicked', buttonId)
+      }
+    }
+  })
 }
--- src/components/Switcher.js
+++ src/components/Switcher.js
@@ -1,19 +1,29 @@
+/**
+ * Switcher component.
+ *
+ * This is stacked buttons group that holds one state.
+ * State is switched by clicking on some button by listening event
+ * on self.
+ *
+ * @param AbstractButtonGroup
+ * @constructor
+ */
 export default function CfSwitcher (AbstractButtonGroup) {
-    return AbstractButtonGroup.extend({
-        template: '#switcher-template',
+  return AbstractButtonGroup.extend({
+    template: '#switcher-template',
 
-        props: {
-            /**
-             * This is component's value, used as
-             * v-model property
-             */
-            value: {},
-        },
+    props: {
+      /**
+       * This is switcher's state, used as
+       * v-model property on <switcher>
+       */
+      value: {},
+    },
 
-        mounted () {
-            this.$on('button-clicked', (state) => {
-                this.$emit('input', state);
-            })
-        }
-    })
+    mounted () {
+      this.$on('button-clicked', (buttonId) => {
+        this.$emit('input', buttonId)
+      })
+    }
+  })
 }
