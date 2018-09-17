f16257b2a620f06b4bc6243d0136fae2132115cc
Roman Lytvynenko <litvinenko95@gmail.com>
2018-09-17 18:25:14 +0300

    lock npm package versions
    EDDBK-104

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,63 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
+var debug = process.env.NODE_ENV !== 'production'
+var webpack = require('webpack')
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 let config = {
-    context: __dirname,
+  context: __dirname,
 
-    devtool: debug ? "inline-sourcemap" : false,
-    plugins: debug ? [] : [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-          sourcemap: false,
-          compress: {
-            warnings: false,
-            drop_console: true
-          },
-        }),
-    ],
-    module: {
-        rules: [
-            { test: /\.js$/, loader: "babel-loader" }
-        ]
+  devtool: debug ? 'inline-sourcemap' : false,
+  plugins: debug ? [
+    new ExtractTextPlugin('styles.css'),
+    new VueLoaderPlugin()
+  ] : [
+    new ExtractTextPlugin('styles.css'),
+    new VueLoaderPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    // new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // new BundleAnalyzerPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      sourcemap: false,
+      compress: {
+        warnings: false,
+        drop_console: true
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js',
+      '@rebelcode/std-lib': '@rebelcode/std-lib/dist/std-lib.umd.js',
     },
-    entry: "./src/app.js",
-    output: {
-        path: __dirname + "/dist/js",
-        filename: "app.min.js",
-        libraryTarget: 'umd'
-    }
-};
+  },
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/dist/js',
+    filename: 'app.min.js',
+    libraryTarget: 'umd'
+  }
+}
 
-module.exports = config;
+module.exports = config
